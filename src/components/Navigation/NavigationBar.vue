5a3c41cbe2cef61f32acf9aacee86c87cb1c8f10
Alexandr Lukovnikov <alexandr.lukovnikov@whenspeak.ru>
2019-10-14 16:26:09 +0300

    add navigation

---
+++ src/components/Navigation/NavigationBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info" tabs>
+    <b-navbar-brand>Test</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="modules">Счета</b-nav-item>
+        <b-nav-item to="control">Операционные дни</b-nav-item>
+        <b-nav-item to="control">Операции</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+import {
+  BNavbar,
+  BCollapse,
+  BNavbarNav,
+  BNavbarToggle,
+  BNavbarBrand,
+} from 'bootstrap-vue';
+
+export default {
+  name: 'NavigationBar',
+  components: {
+    BNavbar,
+    BCollapse,
+    BNavbarNav,
+    BNavbarToggle,
+    BNavbarBrand,
+  },
+};
+</script>
